3c12d287eee25e46419e46b58b594bf432cf68a2
ShreyasK47 <shreyaskarevalli1@gmail.com>
2025-09-06 23:12:12 +0530

    Changes: Clerk Authentication & Routing Setup Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { PublicLayout } from "@/layouts/public-layout";
+import { HomePage } from "@/routes/home";
+
+import { AuthenticationLayout } from "@/layouts/auth-layout";
+import { SignUpPage } from "./routes/sign-up";
+import { SignInPage } from "./routes/sign-in";
+import { ProtectedRoutes } from "./layouts/protected-routes";
+import { MainLayout } from "./layouts/main-layout";
 
 const App = () => {
-  return( 
-    <div className="text-blue-500">
-    App
-    </div>
+  return (
+    <Router>
+      <Routes>
+        {/*public routes*/}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        {/* authentication routes */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+
+        {/*private routes*/}
+        <Route element={
+          <ProtectedRoutes>
+            <MainLayout />
+            </ProtectedRoutes>
+          }>
+
+          {/* add all th protected routes here */ }
+
+        </Route>
+      </Routes>
+    </Router>
   )
 };
 
---
+++ src/layouts/auth-layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom'
+
+export const AuthenticationLayout = () => {
+  return (
+    <div className="w-screen h-screen overflow-hidden flex items-center justify-center relative"> 
+        <img 
+          src="/assets/img/bg.png" 
+          className='absolute w-full h-full object-cover opacity-20'
+          alt="auth-bg"/>
+        <Outlet />
+    </div>
+  )
+};
+
+   
